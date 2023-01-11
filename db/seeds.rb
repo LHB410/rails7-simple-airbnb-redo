82a5aa1a4a70e39f80aabf7583a93e9123bcebc7
Laura Brooks <laura.brooks0@outlook.com>
2023-01-11 13:10:26 +0900

    backend done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "cleaning db"
+Flat.destroy_all
+
+flat_images = ['https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200', "https://images.unsplash.com/photo-1588091945254-e2191fefe7b7", "https://images.unsplash.com/photo-1564137799581-baca1aa17f5d", "https://images.unsplash.com/photo-1533779283484-8ad4940aa3a8", "https://images.unsplash.com/photo-1505873242700-f289a29e1e0f", "https://images.unsplash.com/photo-1560448204-e02f11c3d0e2", "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267", "https://images.unsplash.com/photo-1545158535-c3f7168c28b6", "https://images.unsplash.com/photo-1545158535-c3f7168c28b6", "https://images.unsplash.com/photo-1551927411-95e412943b58" ]
+
+10.times do
+  flat = Flat.create(
+    name: Faker::Adjective.positive.capitalize + " " + ["Apartment", "Flat", "House", "Space", "Getaway"].sample,
+    address: Faker::Address.street_address + " , " + Faker::Address.postcode,
+    description: ["Beautiful and Light", "Small but amazingly comfortable", "Wonderful space with plenty of amenities"].sample,
+    price_per_night: rand(30..300),
+    number_of_guests: [1, 2, 4, 6, 8, 10].sample,
+    picture_url: flat_images.pop
+  )
+
+
+  puts "Created flat #{flat.id}"
+
+end
